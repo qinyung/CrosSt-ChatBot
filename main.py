1f7d40b60f9e0fa31f2023f14275c41c1cc39e38
sprinklelive <67997337+sprinklelive@users.noreply.github.com>
2022-02-10 22:49:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 import websocket
 ws = websocket.WebSocket()
 ws.connect("wss://ws.crosst.chat:35197/")
-ws.send(json.dumps({'cmd': 'join', 'nick': 'dx_xb', 'password': 'tc101217', "clientName": '[Sprinkle Chat](https://pntang.github.io/)', "clientKey": 'Z1ozsN2ZExhhUHt', 'channel': '公共聊天室' }))
+ws.send(json.dumps({'cmd': 'join', 'nick': 'dx_xb', 'password': '1234567', "clientName": '[Sprinkle Chat](https://pntang.github.io/)', "clientKey": 'Z1ozsN2ZExhhUHt', 'channel': '公共聊天室' }))
 ws.send(json.dumps({'cmd': 'chat', 'text': '(｡･∀･)ﾉﾞ嗨'}))
 while 1 == 1:
     msg = str(ws.recv())
