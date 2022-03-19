17642d5673a63e6e0015f2669c13cf76cd128b1c
Sprinkle <65575548+pntang@users.noreply.github.com>
2022-03-19 09:40:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo apt install python3-pip
 pip3 install websocket-client
 wget https://raw.githubusercontent.com/pntang/dx_xb/main/main.py
-echo ------------------RUN <nohup python main.py> TO LAUNCH THIS BOT---------------------
+echo ------------------RUN nohup python main.py TO LAUNCH THIS BOT---------------------
