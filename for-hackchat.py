ba230c26aaa4a9aad2a3fedde94ecb9e78f5fb1b
Sprinkle Tang <65575548+pntang@users.noreply.github.com>
2022-04-05 12:14:46 +0800

    Update for-hackchat.py

--- for-hackchat.py
+++ for-hackchat.py
@@ -11,18 +11,19 @@ import platform
 bot_name = 'SprinkleBot'
 password = '123456'
 channel = 'your-channel'
-# 提取系统信息
-OS_info = platform.platform()
+
+bz = '''
+### SprinkleBot 帮助
+命令列表
+| 表情包 | 趣站 | 涩图 | 蛤 |  ﾟ∀ﾟ)σ |
+'''
 # 加入和发送函数
 def join(bot_name, password, channel):
     ws.send(json.dumps({'cmd': 'join', 'nick': bot_name, 'pass': password, 'channel': channel }))
 
 def send(message):
     ws.send(json.dumps({'cmd': 'chat', 'text': message}))
-# 爬取帮助文件
-URL = 'https://sprinkle.eu.org/dx_xb/help.md'
-res = requests.get(URL)
-res.encoding = 'utf-8'
+
 # 功能列表
 emprs_list = ['( ﾟ∀。)', '(ノﾟ∀ﾟ)ノ', ' ﾟ∀ﾟ)σ', '(*ﾟーﾟ)', '( ﾟ∀ﾟ)', 'σ`∀´) ﾟ∀ﾟ)σ', '(((　ﾟдﾟ)))']
 
@@ -65,8 +66,18 @@ while 1 == 1:
     print(msg)
     emprs = random.choice(emprs_list)
     site = random.choice(site_list)
-    if '@SprinkleBot' in msg:
-        send(res.text)
+    if 'onlineAdd' in msg:
+        if '"nick":"Sprinkle"' in msg and '"trip":"EoZ5HO"' in msg:
+            send('$$\color{red}主\color{orange}人\color{yellow}早\color{green}上\color{blue}好\color{purple}( ﾟ∀。)$$')
+        else:
+            send('偶哈哟，欢迎来到hackchat！(ノﾟ∀ﾟ)ノ')
+            send('输入"帮助"来查看帮助内容')
+    elif 'onlineRemove' in msg:
+        send('拜拜(ノﾟ∀ﾟ)')
+    elif '帮助' in msg and bot_name not in msg:
+        send(bz)
+    elif '涩图' in msg and bot_name not in msg:
+        send('涩图一张，注意身体( ﾟ∀ﾟ) ![waifu](https://pic.sprinkle.workers.dev)')
     elif '表情包' in msg and bot_name not in msg:
         send(emprs)
     elif '趣站' in msg and bot_name not in msg:
@@ -75,7 +86,7 @@ while 1 == 1:
         send('σ`∀´) ﾟ∀ﾟ)σ')
     elif ' ﾟ∀ﾟ)σ' in msg and bot_name not in msg:
         send('σ`∀´) ﾟ∀ﾟ)σ')
-    elif 'bot出去' in msg and '"trip":"bjvk1K"' in msg:
+    elif 'bot出去' in msg and '"trip":"EoZ5HO"' in msg:
         ws.close()
         break
 sys.exit(0)
