da051c294d6eb58e88d85266ad3e2fcfbf0a4a76
Sprinkle Tang <65575548+pntang@users.noreply.github.com>
2022-12-20 10:29:26 +0800

    Update and rename vps-install.sh to install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo apt install python3-pip
-pip3 install websocket-client
+pip3 install -r requirements.txt
 wget https://raw.githubusercontent.com/pntang/dx_xb/main/main.py
 echo ------------------RUN nohup python main.py TO LAUNCH THIS BOT---------------------
