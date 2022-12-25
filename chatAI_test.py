81e2f0d01e7a5d9d419246e506c927e6a6ce2f56
Sprinkle Tang <65575548+pntang@users.noreply.github.com>
2022-12-26 00:13:48 +0800

    Delete chatAI_test.py
    its now useless,i think

--- chatAI_test.py
+++
@@ -1,8 +0,0 @@
-import requests
-import urllib
-def qingyunke(msg):
-    url = 'http://api.qingyunke.com/api.php?key=free&appid=0&msg={}'.format(urllib.parse.quote(msg))
-    html = requests.get(url)
-    return html.json()["content"]
-res = qingyunke(str(input('>')))
-print("青云客>>", res)
